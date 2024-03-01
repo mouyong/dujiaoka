94538842cbcfc29fa852dcc97e4885b4d6f15a1f
mouyong <my24251325@gmail.com>
2024-03-01 11:25:03 +0800

    feat: 插件世界支付中心适配

--- resources/views/hyper/layouts/_footer.blade.php
+++ resources/views/hyper/layouts/_footer.blade.php
@@ -4,11 +4,15 @@
         <div class="row">
             <div class="col-md-6">
             	<div class="footer-links">
-                    Powered by <a href="https://github.com/assimon/dujiaoka">@独角数卡.</a>
+                    Powered by <a href="https://paycenter.plugins-world.cn">@ 插件世界.</a>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="text-md-right footer-links d-none d-md-block">
+                    <a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">蜀ICP备17018093号</a><br><br>
+                    <a href="https://discuss.plugins-world.cn/" target="_blank">交流社区</a>｜
+                    <a href="https://marketplace.plugins-world.cn/" target="_blank">插件市场</a>｜
+                    <a href="https://ssl.plugins-world.cn/" target="_blank">SSL 证书订购</a>
                     {!! dujiaoka_config_get('footer') !!}
                 </div>
             </div>
--- resources/views/hyper/layouts/_nav.blade.php
+++ resources/views/hyper/layouts/_nav.blade.php
@@ -3,7 +3,7 @@
         <!-- LOGO -->
         <a href="/" class="topnav-logo" style="float: none;">
             <img src="{{ picture_ulr(dujiaoka_config_get('img_logo')) }}" height="36">
-            <div class="logo-title">{{ dujiaoka_config_get('text_logo') }}</div>
+            <!--<div class="logo-title">{{ dujiaoka_config_get('text_logo') }}</div>-->
         </a>
         <a class="btn btn-outline-primary" href="{{ url('order-search') }}">
             <i class="noti-icon uil-file-search-alt search-icon"></i>
